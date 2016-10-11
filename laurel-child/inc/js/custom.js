e18fe9d129540f8ca3dbc3d54e1362d351cac745
Michael Greenwood <michael.greenwood@redacademy.com>
2016-10-11 11:33:05 -0400

    Remove old child files

--- laurel-child/inc/js/custom.js
+++ laurel-child/inc/js/custom.js
@@ -31,18 +31,4 @@ jQuery(window).load(function() {
     return false;
   });
 
-  jQuery('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = jQuery(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        jQuery('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
 });
